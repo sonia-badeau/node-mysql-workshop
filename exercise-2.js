afa6fb4441d5775aba427e590b5f68c9dcac82a0
Sonia <sonia.badeau@gmail.com>
2016-02-11 20:35:45 +0000

    Exercise1

--- exercise-2.js
+++ exercise-2.js
@@ -7,7 +7,9 @@ var connection = mysql.createConnection({
 });
 
 connection.query('select id, email from Account limit 5', function(err, results) {
-    results.forEach(function(results){
+   /* results.forEach(function(results){
    console.log('#' + results.id + ': ' + results.email);
-    });
+    });*/
+    console.log(typeof results);
 });
+connection.end();
